909e9593394a1c9c18a83deba53163de4f5e1e32
riverman56 <vex@vexitorylabs.com>
2021-06-14 21:00:45 -0500

    why did i forget this

--- src/networking/init.lua
+++ src/networking/init.lua
@@ -1,7 +1,11 @@
 local request = require(script.request)
 local listen = require(script.listen)
+local registerRoutes = require(script.registerRoutes)
+local createRoute = require(script.createRoute)
 
 return {
     request = request,
     listen = listen,
+    registerRoutes = registerRoutes,
+    createRoute = createRoute,
 }
